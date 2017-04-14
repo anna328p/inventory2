9267287a46335d184d74deecd0d50139f7d1b01f
Dmitry Kudriavtsev <me@dk0.us>
2017-04-13 22:28:47 -0700

    Changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170413052806) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "item_types", force: :cascade do |t|
     t.string   "code"
     t.string   "name"
@@ -59,8 +62,8 @@ ActiveRecord::Schema.define(version: 20170413052806) do
     t.string   "image"
     t.boolean  "admin"
     t.boolean  "guest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end

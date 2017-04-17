4262323f293068053835ced5cc73f12a6c7dd8c0
Dmitry Kudriavtsev <me@dk0.us>
2017-04-17 00:10:01 -0700

    last minute bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  resources :items
+	resources :items
 
 	get 'transactions/check_out'
 	get 'transactions/check_in'
-  resources :transactions
+	resources :transactions
 
-  get 'admin/index'
+	get 'admin/index'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-	get 'users/list'
+	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 	resources :users
 
 	get 'index/index'
 
 	root to: 'index#index'
 
-  resources :item_types
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :item_types
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

a527965f4e6ee9939a545a72becee75bc899525b
Dmitry Kudriavtsev <me@dk0.us>
2017-04-25 01:45:55 -0700

    Fixed user page and text boxes
    Switched to jQuery 2.x which fixed text box labels and select boxes.
    Fixed 500 errors on user page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
 		@users = User.all
 		authorize! :manage, @users
 	end
+	def update
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html { redirect_to @user, notice: 'Item was successfully updated.' }
+				format.json { render :show, status: :ok, location: @user }
+			else
+				format.html { redirect_to @user, notice: 'User could not be updated.' }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 end
